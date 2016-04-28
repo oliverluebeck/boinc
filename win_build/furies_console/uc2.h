20306ce33188d131b6bf4512fbb2dc329ba22083
Oliver Luebeck <oliver_luebeck@hotmail.com>
2016-04-28 18:55:08 +0200

    something something ordinal

---
+++ win_build/furies_console/uc2.h
@@ -0,0 +1,22 @@
+#include "boinc_api.h"
+
+struct UC_SHMEM {
+	double update_time;
+	double fraction_done;
+	double cpu_time;
+	BOINC_STATUS status;
+	int countdown;
+	// graphics app sets this to 5 repeatedly,
+	// main program decrements it once/sec.
+	// If it's zero, don't bother updating shmem
+};
+
+#ifdef _WIN32
+
+extern "C" {
+
+	__declspec(dllimport)  char* uc2_dll_version();
+
+};
+
+#endif
